96f81e23b3e78b5490cb1d5338b320a70d1005c0
arku3 <arkuarku@gmail.com>
2024-09-20 14:36:59 +0800

    Add scrypt example and OWASP parameters config (#7)
    * add scrypt implementation in Node.js-ESM
    
    * setup Bun and add owasp cheat sheet recommended config
    
    * add OWASP recommended config in PHP

--- Bun/argon2.js
+++ Bun/argon2.js
@@ -1,7 +1,20 @@
+// Configuration Reference:
+// https://cheatsheetseries.owasp.org/cheatsheets/Password_Storage_Cheat_Sheet.html#argon2id
+
 // Hash
-const hash = await Bun.password.hash("INPUT_PASSWORD");
+// For synchronous hashing, use Bun.password.hashSync
+const hash = await Bun.password.hash("INPUT_PASSWORD", {
+  algorithm: "argon2id", // Optional. Default is "argon2id"
+  memoryCost: 19456, // Optional, 19 MiB. Default is 65536 (64MiB)
+  timeCost: 2, // Optional. Default is 2
+  parallelism: 1, // Optional. Default is 1
+});
 console.log(hash);
 
 // Verify
+// For synchronous verification, use Bun.password.verifySync
 const result = await Bun.password.verify("INPUT_PASSWORD", hash);
 console.log(result);
+
+const result2 = await Bun.password.verify("BAD_PASSWORD", hash);
+console.log(result2);
