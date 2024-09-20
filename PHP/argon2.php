bf6f7311e003adc14bdbfebba6ed487aeae06fcc
HM Ku <hmku@yakoo.com.hk>
2024-09-20 13:16:15 +0800

    add OWASP recommended config in PHP

--- PHP/argon2.php
+++ PHP/argon2.php
@@ -1,9 +1,19 @@
 <?php
-// Hash
-$hash = password_hash("INPUT_PASSWORD", PASSWORD_ARGON2ID);
-echo $hash;
+// Configuration Reference:
+// https://cheatsheetseries.owasp.org/cheatsheets/Password_Storage_Cheat_Sheet.html#argon2id
 
+// Hash
+// Argon2id available since PHP 7.3.0
+$hash = password_hash("INPUT_PASSWORD", PASSWORD_ARGON2ID, [
+  'memory_cost' => 19456, // m=19456. Optional. Default is PASSWORD_ARGON2_DEFAULT_MEMORY_COST
+  'time_cost' => 2, // t=2. Optional. Default is PASSWORD_ARGON2_DEFAULT_TIME_COST
+  'threads' => 1, // p=1. Optional. Default is PASSWORD_ARGON2_DEFAULT_THREADS
+]);
+echo $hash;
 echo "\n";
 
 // Verify
-echo password_verify("INPUT_PASSWORD", $hash);
+echo password_verify("INPUT_PASSWORD", $hash) ? 'true' : 'false';
+echo "\n";
+echo password_verify("BAD_PASSWORD", $hash) ? 'true' : 'false';
+echo "\n";
--- PHP/bcrypt.php
+++ PHP/bcrypt.php
@@ -1,12 +1,13 @@
 <?php
 // Hash
-$hash = password_hash("INPUT_PASSWORD", PASSWORD_BCRYPT);
+$hash = password_hash("INPUT_PASSWORD", PASSWORD_BCRYPT, [
+  'cost' => 12, // Optional. Default is 10 for php <= 8.3.x  
+]);
 echo $hash;
-
 echo "\n";
 
 // Verify
-echo password_verify("INPUT_PASSWORD", $hash);
-
-
-
+echo password_verify("INPUT_PASSWORD", $hash,) ? 'true' : 'false';
+echo "\n";
+echo password_verify("BAD_PASSWORD", $hash) ? 'true' : 'false';
+echo "\n";
